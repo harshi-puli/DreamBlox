cfeab9b5be8a202613a730476fec95d6aa39d862
slingann <slingan01@gmail.com>
2025-10-25 18:29:16 -0700

    image description stuff

--- backend/reka_api.py
+++ backend/reka_api.py
@@ -1,15 +1,18 @@
+from reka import ChatMessage
 from reka.client import Reka
 
-# You can also set the API key using the REKA_API_KEY environment variable.
 client = Reka(api_key="7e8df0937435c48a00497457bff8a9c0d279d9e72f43d305e7daef9bf2631bb1")
-
 response = client.chat.create(
     messages=[
-        {
-            "content": "What is the fifth prime number?",
-            "role": "user",
-        }
+        ChatMessage(
+            content=[
+                {"type": "image_url", "image_url": "https://v0.docs.reka.ai/_images/000000245576.jpg"},
+                {"type": "text", "text": "What animal is this? Answer briefly"}
+            ],
+            role="user",
+        )
     ],
     model="reka-core-20240501",
 )
 print(response.responses[0].message.content)
+
